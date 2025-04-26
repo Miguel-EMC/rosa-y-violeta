305853d736a4cb417898decb88e17da325eb1630
miguel <eduardomuzo123456@gmail.com>
2025-04-25 23:44:45 -0500

    Update

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
     // apiUrl: 'https://banco-politecnico.online/api/',
-    apiUrl: 'http://127.0.0.1:8000/api/',
+    apiUrl: 'http://64.23.255.65/api/',
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     // apiUrl: 'https://banco-politecnico.online/api/',
-    apiUrl: 'http://127.0.0.1:8000/api/',
+    apiUrl: 'http://64.23.255.65/api/',
 
 };
